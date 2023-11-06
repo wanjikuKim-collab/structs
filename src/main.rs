b15d214d5837fc53522e44e6982a2ad90c5d1d61
wanjikuKim-collab <wanjikukimani24@gmail.com>
2023-11-06 13:58:29 +0300

    Defining methods

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
   // refactoring with structs
-  struct Rectangle{
+  #[derive(Debug)]
+struct Rectangle{
     width: u32,
     height: u32,
 } 
-fn main(){
 
+impl Rectangle {
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+    
+}
+fn main(){
     let rect1= Rectangle{
-        width: 10,
+        width: 20,
         height: 20,
     };
  
-    println!("The area of the rectangle is :{}", area(&rect1));
- }
- 
- fn area(rectangle: &Rectangle)->u32{
-    rectangle.width * rectangle.height
+    println!("The area of the rectangle is :{}", rect1.area());
  }
